ac0cbc7710e648993ffbb8c7ba7daae6f103c166
master <shyngys99106@gmail.com>
2020-07-19 12:47:07 +0600

    first commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-trailing-spaces */
+import countTimer from '../modules/countTimer';
+import toggleMenu from '../modules/toggleMenu';
+import togglePopUp from '../modules/togglePopUp';
+import tabs from '../modules/tabs';
+import slider from '../modules/slider';
+import changePhoto from '../modules/changePhoto';
+import calc from '../modules/calculater';
+import sendForm from '../modules/sendForm';
+
+
+countTimer('9 july 2020');
+
+toggleMenu();
+
+togglePopUp();
+
+tabs();
+    
+slider();
+
+changePhoto();
+
+calc();
+     
+sendForm(); 
+
